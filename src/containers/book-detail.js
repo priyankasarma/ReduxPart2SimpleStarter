2853da4199df2a9a1cb9b9d0bba1c6e3d370fb88
Priyanka Sarma <priyanka.sarma@3plearning.com>
2017-07-03 17:18:14 +1000

    Completed video 49

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -4,8 +4,16 @@ import  { connect } from 'react-redux';
 
 class BookDetail extends Component {
     render (){
+        if (!this.props.book){
+            return <div><h3>Select a book to see the details.</h3></div>;
+        }
+
         return (
-            <div>Book Detail</div>
+            <div>
+                <h3>Details for:</h3>
+                <div>{ this.props.book.title }</div>
+                <div>{ this.props.book.pages }</div>
+            </div>
         );
     }
 }
--- src/reducers/books_reducer.js
+++ src/reducers/books_reducer.js
@@ -1,8 +1,8 @@
 export default (currentState, action) => {
     return [
-        { title: 'Harry Potter' },
-        { title: 'Lord of the Rings' },
-        { title: 'Hitchhikers Guide To the Galaxy' },
-        { title: 'Artemis Fowl' },
+        { title: 'Harry Potter', pages: 500 },
+        { title: 'Lord of the Rings', pages: 700 },
+        { title: 'Hitchhikers Guide To the Galaxy', pages: 400 },
+        { title: 'Artemis Fowl', pages: 300 },
     ];
 }
