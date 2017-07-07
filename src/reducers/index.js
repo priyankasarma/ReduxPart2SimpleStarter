155b1a3205a258d59b9c24be272597d9993efa64
Priyanka Sarma <priyanka.sarma@3plearning.com>
2017-07-07 14:43:48 +1000

    Renamed the activeBookReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './books_reducer';
-import ActiveBook from './active_book_reducer';
+import ActiveBookReducer from './active_book_reducer';
 
 const rootReducer = combineReducers({
   books: BooksReducer,
-  activeBook: ActiveBook
+  activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
